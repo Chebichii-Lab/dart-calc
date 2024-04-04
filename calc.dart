eac00e7d05f55f2d741f852e65919e46dc591cb3
Chebichii-Lab <chebichiiserem@gmail.com>
2024-04-04 18:14:51 +0300

    add mathematical operations

--- calc.dart
+++ calc.dart
@@ -1,4 +1,5 @@
 import 'dart:io';
+import 'dart:math';
 
 void main() {
   print('Nats Calculator');
@@ -21,7 +22,47 @@ void main() {
   print('9. Floor Division');
   print('10. Logarithm(log())');
 
-  String? choice = stdin.readLineSync();
+  int choice = int.parse(stdin.readLineSync()!);
 
-  double result;
+   switch (choice) {
+    case 1:
+      print('$num1 + $num2 = ${num1 + num2}');
+      break;
+    case 2:
+      print('$num1 - $num2 = ${num1 - num2}');
+      break;
+    case 3:
+      if (num2 != 0) {
+        print('$num1 / $num2 = ${num1 / num2}');
+      } else {
+        print('Error: Division by zero');
+      }
+      break;
+    case 4:
+      print('$num1 * $num2 = ${num1 * num2}');
+      break;
+    case 5:
+      print('$num1 ^ $num2 = ${pow(num1, num2)}');
+      break;
+    case 6:
+      print('$num1 % $num2 = ${num1 % num2}');
+      break;
+    case 7:
+      print('Square root of $num1 = ${sqrt(num1)}');
+      print('Square root of $num2 = ${sqrt(num2)}');
+      break;
+    case 8:
+      print('Absolute value of $num1 = ${num1.abs()}');
+      print('Absolute value of $num2 = ${num2.abs()}');
+      break;
+    case 9:
+      print('$num1 ~/ $num2 = ${num1 ~/ num2}');
+      break;
+    case 10:
+      print('Logarithm of $num1 = ${log(num1)}');
+      print('Logarithm of $num2 = ${log(num2)}');
+      break;
+    default:
+      print('Invalid choice');
+  }
 }
